3fd2a616af3de8aed9f4f4713dd1dfd0930dee29
ajid-oss <zakarie.edan1@gmail.com>
2026-01-06 10:25:32 +0300

    Adding Exercise 1 and  screenshots

---
+++ ExerciseOne/src/App.jsx
@@ -0,0 +1,9 @@
+import UserCard from './UserCard';
+
+function App() {
+  return (
+      <UserCard/>
+  )
+}
+
+export default App
---
+++ ExerciseOne/src/UserCard.jsx
@@ -0,0 +1,9 @@
+export const UserCard = () => {
+    return (
+    <div>
+        <h2> John Doe</h2>
+        <p> john.doe@example.com</p>
+    </div>
+    )
+}
+export default UserCard;
