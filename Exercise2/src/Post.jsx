3dac078a8535f80a8dd6fc117b4b1770a8db0703
ajid-oss <zakarie.edan1@gmail.com>
2026-01-06 11:00:50 +0300

    Adding Exercise 2 and  screenshots

---
+++ Exercise2/src/Post.jsx
@@ -0,0 +1,10 @@
+const Post = () => {
+  return (
+    <article>
+      <h2>Title: Learn to be Alone</h2>
+      <p>Sometimes the best growth happens in silence. You do not always need someone beside you to enjoy life. Learn to go out alone, sit with your own thoughts, and understand who you really are.</p>
+    </article>
+  );
+};
+
+export default Post;
