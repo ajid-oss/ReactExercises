000faafe122cc07f4dc539ec7cf15557d75ca0b6
ajid-oss <zakarie.edan1@gmail.com>
2026-01-14 09:40:33 +0300

    Adding Exercise3 and screeshot

---
+++ Exercise3/src/App.jsx
@@ -0,0 +1,29 @@
+import UserCard from './UserCard';
+
+function App() {
+  return (
+    <>
+      <UserCard 
+      Name={"Saki Ahmed"} 
+      Email={"sakiahmed@gmail.com"}
+      />
+
+      <UserCard 
+      Name={"Abdifatah Jama"} 
+      Email={"Abdi.jama@gmail.com"}
+      />
+
+      <UserCard 
+      Name={"Fatima Ahmed"} 
+      Email={"fatimamaed@gmail.com"}
+      />
+
+      <UserCard 
+      Name={"Caasha Mohamed"} 
+      Email={"Aishamohamed@gmail.com"}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ Exercise3/src/UserCard.jsx
@@ -0,0 +1,10 @@
+const UserCard = ({Name, Email}) =>  {
+    return (
+    <>
+    <h1>Hello Welcome {Name}</h1>
+    <h2>Your name is: {Name} </h2>
+    <h2>You email is: {Email}</h2>
+    </>
+    )
+}
+export default UserCard;
